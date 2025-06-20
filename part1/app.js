172a37a24e54dc0d2fea0eb925047577d262dbbf
HastumeM <simbagsps@gmail.com>
2025-06-20 11:05:51 +0000

    [automatic commit] 2025-06-20 11:05:51 UTC 119.40.107.144

--- part1/app.js
+++ part1/app.js
@@ -58,7 +58,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [walkers] = await db.execute(`SELECT u.username AS walker_username, COUNT(DISTINCT wr.rating_id) AS total_ratings, AVG(wr.rating) AS average_rating, (SELECT COUNT(*) FROM WalkRequests wq WHERE wq.status = 'completed' AND EXISTS ())`);
+    const [walkers] = await db.execute(`SELECT u.username AS walker_username, COUNT(DISTINCT wr.rating_id) AS total_ratings, AVG(wr.rating) AS average_rating, (SELECT COUNT(*) FROM WalkRequests wq WHERE wq.status = 'completed' AND EXISTS (SELECT 1 FROM WalkApplications wa WHERE wa.request_id = sq))`);
     res.json(walkers);
   } catch (err) {
     res.status(500).json({ error: 'Failed to walker summary' });
