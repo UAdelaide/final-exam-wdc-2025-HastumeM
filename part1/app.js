a05230029c3d394781a1d5cfa1afaa6dc3a4748f
HastumeM <simbagsps@gmail.com>
2025-06-20 11:05:09 +0000

    [automatic commit] 2025-06-20 11:05:09 UTC 119.40.107.144

--- part1/app.js
+++ part1/app.js
@@ -58,7 +58,7 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [walkers] = await db.execute(`SELECT u.username AS walker_username, COUNT(DISTINCT wr.rating_id) AS total_ratings, AVG(wr.rating) AS average_rating, (SELECT COUNT())`);
+    const [walkers] = await db.execute(`SELECT u.username AS walker_username, COUNT(DISTINCT wr.rating_id) AS total_ratings, AVG(wr.rating) AS average_rating, (SELECT COUNT(*) FROM WalkRequests wq)`);
     res.json(walkers);
   } catch (err) {
     res.status(500).json({ error: 'Failed to walker summary' });
