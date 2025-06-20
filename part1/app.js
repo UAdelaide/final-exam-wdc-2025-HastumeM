5509d57b892421e53bdb632d209c2d61d8b4c667
HastumeM <simbagsps@gmail.com>
2025-06-20 10:48:16 +0000

    [automatic commit] 2025-06-20 10:48:15 UTC 119.40.107.144

--- part1/app.js
+++ part1/app.js
@@ -49,8 +49,8 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
-    res.json(dogs);
+    const [requests] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
+    res.json(requests);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch walk requests' });
   }
