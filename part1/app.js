1be08e6bc382a30173ba7126e3d2e670a96274f4
HastumeM <simbagsps@gmail.com>
2025-06-20 10:36:52 +0000

    [automatic commit] 2025-06-20 10:36:52 UTC 119.40.107.144

--- part1/app.js
+++ part1/app.js
@@ -40,7 +40,7 @@ let db;
 
 app.get('/api/dogs', async (req, res) => {
   try {
-    const [books] = await db.execute('SELECT * FROM books');
+    const [dogs] = await db.execute('SELECT * FROM books');
     res.json(books);
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch books' });
