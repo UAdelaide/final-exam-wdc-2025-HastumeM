6686ea903ec9267ae50655cd1cf444199434e6c0
HastumeM <simbagsps@gmail.com>
2025-06-20 10:48:37 +0000

    [automatic commit] 2025-06-20 10:48:36 UTC 119.40.107.144

--- part1/app.js
+++ part1/app.js
@@ -58,8 +58,8 @@ app.get('/api/walkrequests/open', async (req, res) => {
 
 app.get('/api/walkers/summary', async (req, res) => {
   try {
-    const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
-    res.json(dogs);
+    const [walkers] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
+    res.json(walkers);
   } catch (err) {
     res.status(500).json({ error: 'Failed to walker summary' });
   }
