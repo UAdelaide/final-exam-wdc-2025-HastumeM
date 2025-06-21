2a25f206a178223981166714a0080eddf30199b3
HastumeM <simbagsps@gmail.com>
2025-06-21 02:43:32 +0000

    [automatic commit] 2025-06-21 02:43:32 UTC 119.40.107.144

--- part2/app.js
+++ part2/app.js
@@ -32,9 +32,8 @@ app.get('/api/dogs', async (req, res) => {
         res.json(dogs);
     } else {
         const [dogs] = await db.execute('SELECT d.name AS dog_name, d.size, u.username AS owner_username FROM Dogs d JOIN Users u ON d.owner_id = u.user_id');
-    res.json(dogs);
+        res.json(dogs);
     }
-
   } catch (err) {
     res.status(500).json({ error: 'Failed to fetch dogs' });
   }
